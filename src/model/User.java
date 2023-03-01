39f1a35a68b31b10891efcb4979d681e0445fd92
SakthiVijayS <124193377+SakthiVijayS@users.noreply.github.com>
2023-03-01 21:51:36 +0530

    Add files via upload
    Basic Implementation

---
+++ src/model/User.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.LocalDate;
+import java.util.Date;
+import javax.swing.ImageIcon;
+import java.util.UUID;
+
+/**
+ *
+ * @author shakt
+ */
+public class User {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = UUID.randomUUID().toString();
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getTeleNo() {
+        return teleNo;
+    }
+
+    public void setTeleNo(long teleNo) {
+        this.teleNo = teleNo;
+    }
+
+    public ImageIcon getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(ImageIcon profilePic) {
+        this.profilePic = profilePic;
+    }
+    String name;
+    String employeeId;
+    int age;
+    String gender;
+    LocalDate startDate;
+    String level;
+    String email;
+    long teleNo;
+    ImageIcon profilePic;
+    
+    
+}
---
+++ src/model/UserDirectory.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author shakt
+ */
+public class UserDirectory {
+    ArrayList<User> allUsers;
+
+    public UserDirectory() {
+        this.allUsers = new ArrayList<>();
+    }
+    
+    
+
+    public ArrayList<User> getAllUsers() {
+        return allUsers;
+    }
+
+    public void setAllUsers(ArrayList<User> allUsers) {
+        this.allUsers = allUsers;
+    }
+    
+      public void addUser(User newUser){
+        this.allUsers.add(newUser);
+    }
+    
+    public void removeUser(User user){
+        this.allUsers.remove(user);
+    }
+
+    @Override
+    public String toString() {
+        return "UserDirectory{" + "allUsers=" + allUsers + '}';
+    }
+    
+    
+}
